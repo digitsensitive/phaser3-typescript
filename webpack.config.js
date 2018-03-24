48a7f123e00c3ae25ceefa4fa24ff47f2d9b5dcc
digitsensitive <digit.sensitivee@gmail.com>
2018-03-24 20:56:38 +0100

    small fix in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
 var phaser = path.join(pathToPhaser, 'dist/phaser.js');
 
 module.exports = {
-  entry: './src/games/endlessRunner/endlessRunner.ts',
+  entry: './src/boilerplate/game.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
