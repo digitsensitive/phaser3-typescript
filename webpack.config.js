4e1b9e0534595e455c4ca319a53e88c37658db6c
digitsensitive <digit.sensitivee@gmail.com>
2018-10-23 16:12:57 +0200

    path not showing to boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
 var phaser = path.join(pathToPhaser, 'dist/phaser.js');
 
 module.exports = {
-  entry: './src/games/flappy-bird/game.ts',
+  entry: './src/boilerplate/game.ts',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
