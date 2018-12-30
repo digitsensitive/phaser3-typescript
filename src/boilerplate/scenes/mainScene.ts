1b6afa93918fa34bcf51ea23f8e189df42d488f3
digitsensitive <digit.sensitivee@gmail.com>
2018-12-30 20:26:12 +0100

    update mainScene of boilerplate

--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -6,7 +6,6 @@
 
 export class MainScene extends Phaser.Scene {
   private phaserSprite: Phaser.GameObjects.Sprite;
-  private container: Phaser.GameObjects.Container;
 
   constructor() {
     super({
@@ -20,15 +19,5 @@ export class MainScene extends Phaser.Scene {
 
   create(): void {
     this.phaserSprite = this.add.sprite(400, 300, "logo");
-    this.container = this.add.container(0, 0);
-
-    this.container.add(this.phaserSprite);
-    console.log(this.container.getAll());
-    this.container.each(function(child) {
-      child.destroy();
-    });
-    console.log(this.container.getAll());
   }
-
-  update(): void {}
 }
