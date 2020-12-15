2578c834d4e37a403fe3ca2905bcd26634b72067
digitsensitive <digit.sensitivee@gmail.com>
2020-12-15 23:10:42 +0100

    update boilerplate

--- src/boilerplate/scenes/main-scene.ts
+++ src/boilerplate/scenes/main-scene.ts
@@ -1,12 +1,6 @@
 export class MainScene extends Phaser.Scene {
   private mySprite: Phaser.GameObjects.Sprite;
 
-  // Basic sine wave
-  protected amplitude: number = 5;
-  protected frequency: number = 0.5;
-  protected angularFrequency: number = 2 * Math.PI * this.frequency;
-  protected phase: number = 1;
-
   constructor() {
     super({ key: 'MainScene' });
   }
@@ -16,12 +10,6 @@ export class MainScene extends Phaser.Scene {
   }
 
   create(): void {
-    this.mySprite = this.add.sprite(400, 350, 'myTexture');
-  }
-
-  update(time: number): void {
-    this.mySprite.y +=
-      this.amplitude *
-      Math.sin(this.angularFrequency * (time / 1000) + this.phase);
+    this.mySprite = this.add.sprite(400, 300, 'myTexture');
   }
 }
