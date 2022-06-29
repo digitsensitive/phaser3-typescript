281ff876fcdd23afd4e87cdf44a5ae7bece87f9b
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2022-06-29 23:55:58 +0700

    update candy crush

--- src/games/candy-crush/src/objects/tile.ts
+++ src/games/candy-crush/src/objects/tile.ts
@@ -1,3 +1,4 @@
+import { CONST } from '../const/const';
 import { IImageConstructor } from '../interfaces/image.interface';
 
 export class Tile extends Phaser.GameObjects.Image {
@@ -7,7 +8,39 @@ export class Tile extends Phaser.GameObjects.Image {
     // set image settings
     this.setOrigin(0, 0);
     this.setInteractive();
-
+    // this.initTween();
     this.scene.add.existing(this);
+  }
+
+  public initTween(){
+    this.setScale(0,0);
+    this.setAngle(-180)
+    this.scene.tweens.add({
+      targets: this,
+      scaleX: 1,
+      scaleY: 1,
+      angle: 0,
+      ease: 'Sine.easeInOut',
+      duration: 1000,
+      delay: (this.x / CONST.tileWidth) * 50,
+      repeat: 0,
+      yoyo: false,
+  });
+  }
+
+  public initTweenMatch(){
+    this.scene.tweens.add({
+      targets: this,
+      scaleX: 1.5,
+      scaleY: 1.5,
+      angle: 0,
+      ease: 'Sine.easeInOut',
+      duration: 1000,
+      repeat: 0,
+      yoyo: false,
+      onComplete:()=>{
+        this.destroy();
+      }
+  });
   }
 }
