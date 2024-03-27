c7d303ad59127442655a56f0c428e4f3c039f621
digitsensitive <digit.sensitivee@gmail.com>
2024-03-27 14:54:24 +0100

    remove phaser in module->rules in webpack config

--- src/experimental/cellular-automaton/webpack.config.ts
+++ src/experimental/cellular-automaton/webpack.config.ts
@@ -19,11 +19,6 @@ const config: webpack.Configuration = {
         test: /\.ts$/,
         include: path.resolve(__dirname, 'src'),
         loader: 'ts-loader'
-      },
-      {
-        test: require.resolve('Phaser'),
-        loader: 'expose-loader',
-        options: { exposes: { globalName: 'Phaser', override: true } }
       }
     ]
   },
